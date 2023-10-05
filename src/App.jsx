aa30234578e7f20195c373e232a54ab50bc24210
bahare <pondblue07@gmail.com>
2023-10-05 14:58:02 +0330

    import all pages to App.jsx and use them

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Blog from './pages/blog/Blog'
+import Panel from './pages/panel/Panel'
+import Login from './pages/login/Login'
+import Course from './pages/course/Course'
 function App(){
     return(
-        <>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/blog' element={<Blog />} />
+                <Route path='/panel' element={<Panel />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/course' element={<Course />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 export default App
