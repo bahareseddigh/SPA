7fa58c2e8950630e07f96c05ebeb021bb15dbe71
bahare <pondblue07@gmail.com>
2023-10-06 12:42:55 +0330

    import and use MyNavbar in Course.jsx

--- src/pages/course/Course.jsx
+++ src/pages/course/Course.jsx
@@ -1,7 +1,11 @@
+import MyNavbar from '../../components/navbar/Navbar'
 import './Course.css'
 function Course(){
     return(
+        <>
+        <MyNavbar />
         <h1>Course Page</h1>
+        </>
     )
 }
 export default Course
