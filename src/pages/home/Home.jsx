7de410560e85c6e1181755f842dae288f1648b7c
bahare <pondblue07@gmail.com>
2023-10-06 21:30:19 +0330

    import CourseItem and useState and use them

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,37 @@ import {Container, Row, Col} from 'react-bootstrap'
 import MyNavbar from '../../components/navbar/Navbar'
 import './Home.css'
 import progImg from '../../assets/images/programming.svg'
+import CourseItem from '../../components/course/CourseItem'
+import { useState } from 'react'
 function Home(){
+
+    const [courses, setCourses] = useState([
+        {
+            id : 1,
+            img : 'https://next1code.ir/wp-content/uploads/2022/12/react-cover-500x286.jpg',
+            title : 'دوره ریکت',
+            text : 'آموزش پروژه محور ریکت و ریداکس از مقدماتی تا پیشرفته'
+        },
+        {
+            id : 2,
+            img : 'https://next1code.ir/wp-content/uploads/2022/10/bootstrap-500x286.png',
+            title : 'دوره بوتسترپ',
+            text : 'آموزش بوتسترپ 5.2 پروژه محور با تمرین های زیاد'
+        },
+        {
+            id : 3,
+            img : 'https://next1code.ir/wp-content/uploads/2021/08/couse-cover-500x286.jpg',
+            title : 'دوره جاوا اسکریپت',
+            text : 'آموزش صفر تا صد جاوا اسکریپت به صورت پروژه محور'
+        },
+        {
+            id : 4,
+            img : 'https://next1code.ir/wp-content/uploads/2021/06/rwd-couse-cover-500x286.jpg',
+            title : 'دوره طراحی ریسپانسیو',
+            text : 'آموزش طراحی وب ریسپانسیو با مدیا کویری ها و فلکس باکس'
+        },
+    ])
+
     return(
         <>
         <MyNavbar />
@@ -14,6 +44,21 @@ function Home(){
                 </Col>
                 <Col lg={6} className='py-4'>
                     <img className='img-fluid' src={progImg} />
+                </Col>
+            </Row>
+            <Row className='my-5'>
+                <h2 className='py-4'>دوره آموزشی</h2>
+                <Col>
+                    <CourseItem />
+                </Col>
+                <Col>
+                    <CourseItem />
+                </Col>
+                <Col>
+                    <CourseItem />
+                </Col>
+                <Col>
+                    <CourseItem />
                 </Col>
             </Row>
         </Container>
