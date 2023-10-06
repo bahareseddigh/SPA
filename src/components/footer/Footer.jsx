b465c049bbb5abe8d84504d41660c3f65dd4c26a
bahare <pondblue07@gmail.com>
2023-10-06 22:11:09 +0330

    import Container component, Row and etc & use them

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,15 @@
+import { Col, Container, Row } from "react-bootstrap"
+
 function Footer(){
     return(
-        <></>
+        <footer>
+            <Container>
+                <Row>
+                    <Col></Col>
+                    <Col></Col>
+                </Row>
+            </Container>
+        </footer>
     )
 }
 export default Footer
