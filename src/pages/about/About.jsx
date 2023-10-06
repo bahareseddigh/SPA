13197479079b2b83f28df3d0d09dd6cafe23b12e
bahare <pondblue07@gmail.com>
2023-10-06 12:41:03 +0330

    import and use MyNavbar in About.jsx

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,11 @@
+import MyNavbar from '../../components/navbar/Navbar'
 import './About.css'
 function About(){
     return(
+        <>
+        <MyNavbar />
         <h1>About Page</h1>
+        </>
     )
 }
 export default About
