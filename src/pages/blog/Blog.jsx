498faa9d9971ba2d8d32eb14c57a696dd59d0999
bahare <pondblue07@gmail.com>
2023-10-06 12:41:50 +0330

    import and use MyNavbar in Blog.jsx

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,7 +1,11 @@
+import MyNavbar from '../../components/navbar/Navbar'
 import './Blog.css'
 function Blog(){
     return(
+        <>
+        <MyNavbar />
         <h1>Blog Page</h1>
+        </>
     )
 }
 export default Blog
