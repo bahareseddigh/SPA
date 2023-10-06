9d29adf43f09c5d099e674fd0b4fb8072dbf6f2a
bahare <pondblue07@gmail.com>
2023-10-06 12:43:42 +0330

    import and use MyNavbar in Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,11 @@
+import MyNavbar from '../../components/navbar/Navbar'
 import './Login.css'
 function Login(){
     return(
+        <>
+        <MyNavbar />
         <h1>Login Page</h1>
+        </>
     )
 }
 export default Login
