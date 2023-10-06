4ead9ff9025fce201b2b300b617ced43dd2ca57f
bahare <pondblue07@gmail.com>
2023-10-06 12:44:44 +0330

    import and use MyNavbar in Panel.jsx

--- src/pages/panel/Panel.jsx
+++ src/pages/panel/Panel.jsx
@@ -1,7 +1,11 @@
+import MyNavbar from '../../components/navbar/Navbar'
 import './Panel.css'
 function Panel(){
     return(
+        <>
+        <MyNavbar />
         <h1>Panel Page</h1>
+        </>
     )
 }
 export default Panel
