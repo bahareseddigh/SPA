633c58f25e5a205609c77949b2df129f634ef7c9
bahare <pondblue07@gmail.com>
2023-10-06 12:20:31 +0330

    import and use Navbar.jsx in Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,11 @@
+import MyNavbar from '../../components/navbar/Navbar'
 import './Home.css'
 function Home(){
     return(
+        <>
+        <MyNavbar />
         <h1>Home Page</h1>
+        </>
     )
 }
 export default Home
