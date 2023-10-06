bed44cba3c16f9e4f2b19a6b5cf61d51a4e3bf3f
bahare <pondblue07@gmail.com>
2023-10-06 22:29:29 +0330

    set align items center to footer's row

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
     return(
         <footer>
             <Container>
-                <Row>
+                <Row className="align-items-center">
                     <Col>
                         <h2>رسالت نکست وان کد</h2>
                         <p>نکست وان کد با هدف تولید آموزش هایی ایجاد شده ، که حس شیرینِ یادگیری لذت بخش برنامه نویسی و طراحی وب رو تجربه کنید.</p>
