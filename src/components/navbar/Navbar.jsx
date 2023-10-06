0ec9d654864b182781c420fd914c027ead3e5441
bahare <pondblue07@gmail.com>
2023-10-06 12:35:06 +0330

    set links on navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,18 +2,19 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import { NavLink } from 'react-router-dom';
 
 function MyNavbar(){
     const expand = 'md'
     return(
         <Navbar style={{backgroundColor : 'rgb(186, 182, 253)'}} expand={expand} className="mb-3">
-          <Container fluid>
-            <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+          <Container>
+            <Navbar.Brand style={{fontFamily : 'Lalezar', fontSize : '27px'}} href="#">نکست وان کد</Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
+              placement="start"
             >
               <Offcanvas.Header closeButton>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
@@ -22,8 +23,11 @@ function MyNavbar(){
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
+                  <NavLink to='/' className='nav-link'>صفحه اصلی</NavLink>
+                  <NavLink to='/about' className='nav-link'>درباره ما</NavLink>
+                  <NavLink to='/blog' className='nav-link'>بلاگ</NavLink>
+                  <NavLink to='/panel' className='nav-link'>پنل</NavLink>
+                  <NavLink to='/login' className='nav-link'>ورود</NavLink>
                 </Nav>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
