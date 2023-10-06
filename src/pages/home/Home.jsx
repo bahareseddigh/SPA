b5b84bbaae629a3e09be336606f9aaa5378c6efe
bahare <pondblue07@gmail.com>
2023-10-06 13:48:38 +0330

    import and use 3 components from react-bootstrap

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,16 @@
+import {Container, Row, Col} from 'react-bootstrap'
 import MyNavbar from '../../components/navbar/Navbar'
 import './Home.css'
 function Home(){
     return(
         <>
         <MyNavbar />
-        <h1>Home Page</h1>
+        <Container>
+            <Row>
+                <Col></Col>
+                <Col></Col>
+            </Row>
+        </Container>
         </>
     )
 }
