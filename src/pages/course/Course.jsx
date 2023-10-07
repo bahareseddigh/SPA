cbacfea8e127e97c72961566597b7dcc27ccda3c
bahare <pondblue07@gmail.com>
2023-10-07 23:35:16 +0330

    use useParams in Course.jsx

--- src/pages/course/Course.jsx
+++ src/pages/course/Course.jsx
@@ -1,7 +1,11 @@
 import MyNavbar from '../../components/navbar/Navbar'
 import './Course.css'
 import { courseData } from '../../data'
+import { useParams } from 'react-router-dom'
 function Course(){
+
+    const courseId = useParams().courseId
+
     return(
         <>
         <MyNavbar />
