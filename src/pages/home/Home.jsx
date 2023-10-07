49b150485d2693d3b96e535e2e4abba694fbf546
bahare <pondblue07@gmail.com>
2023-10-07 23:23:36 +0330

    import data.js to Home.jsx and use it

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,34 +5,10 @@ import progImg from '../../assets/images/programming.svg'
 import CourseItem from '../../components/course/CourseItem'
 import { useState } from 'react'
 import Footer from '../../components/footer/Footer'
+import { courseData } from '../../data'
 function Home(){
 
-    const [courses, setCourses] = useState([
-        {
-            id : 1,
-            img : 'https://next1code.ir/wp-content/uploads/2022/12/react-cover-500x286.jpg',
-            title : 'دوره ریکت',
-            text : 'آموزش پروژه محور ریکت و ریداکس از مقدماتی تا پیشرفته'
-        },
-        {
-            id : 2,
-            img : 'https://next1code.ir/wp-content/uploads/2022/10/bootstrap-500x286.png',
-            title : 'دوره بوتسترپ',
-            text : 'آموزش بوتسترپ 5.2 پروژه محور با تمرین های زیاد'
-        },
-        {
-            id : 3,
-            img : 'https://next1code.ir/wp-content/uploads/2021/08/couse-cover-500x286.jpg',
-            title : 'دوره جاوا اسکریپت',
-            text : 'آموزش صفر تا صد جاوا اسکریپت به صورت پروژه محور'
-        },
-        {
-            id : 4,
-            img : 'https://next1code.ir/wp-content/uploads/2021/06/rwd-couse-cover-500x286.jpg',
-            title : 'دوره طراحی ریسپانسیو',
-            text : 'آموزش طراحی وب ریسپانسیو با مدیا کویری ها و فلکس باکس'
-        },
-    ])
+    const [courses, setCourses] = useState(courseData)
 
     return(
         <>
