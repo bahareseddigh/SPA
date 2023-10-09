c98e3aa2d3e4a78d29bdbf38aec79cca5d5e7cae
bahare <pondblue07@gmail.com>
2023-10-09 21:50:11 +0330

    add Routes to Blog.jsx

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom'
 import MyNavbar from '../../components/navbar/Navbar'
 import './Blog.css'
 function Blog(){
@@ -6,6 +7,12 @@ function Blog(){
             <MyNavbar />
             <h1>صفحه مقالات</h1>
             <hr />
+
+            <Routes>
+                <Route path='php' element={<h1>php blog</h1>} />
+                <Route path='js' element={<h1>javascript blog</h1>} />
+                <Route path='react' element={<h1>react blog</h1>} />
+            </Routes>
         </div>
     )
 }
