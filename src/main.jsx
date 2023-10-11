b14ce6f5cbbe96885c406a8623bcf4dd7572ea57
bahare <pondblue07@gmail.com>
2023-10-11 23:47:04 +0330

    import and use BrowserRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import App from './App.jsx'
 import './main.css'
 import 'bootstrap/dist/css/bootstrap.rtl.css'
 import ThemeProvider from 'react-bootstrap/ThemeProvider'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider dir='rtl'>
+      <BrowserRouter>
+      
       <App />
+      </BrowserRouter>
     </ThemeProvider>
 )
