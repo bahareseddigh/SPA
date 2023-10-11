1c0203ad6295ab6979f3d30e99ecc156b9027d0b
bahare <pondblue07@gmail.com>
2023-10-11 23:47:37 +0330

    routes of App.jsx modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, useRoutes} from 'react-router-dom'
 import Home from './pages/home/Home'
 import About from './pages/about/About'
 import Blog from './pages/blog/Blog'
-import Panel from './pages/panel/Panel'
+import PhpBlog from './components/blogItems/PhpBlog'
+import JsBlog from './components/blogItems/JsBlog'
+import ReactBlog from './components/blogItems/ReactBlog'
 import Login from './pages/login/Login'
+import Panel from './pages/panel/Panel'
 import Course from './pages/course/Course'
+
 function App(){
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/about' element={<About />} />
-                <Route path='/blog/*' element={<Blog />} />
-                <Route path='/panel' element={<Panel />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/course/:courseId' element={<Course />} />
-            </Routes>
-        </BrowserRouter>
-    )
+
+    let routes = useRoutes([
+        { path : '/', element : <Home />},
+        { path : '/about', element : <About />},
+        { path : '/blog/*', element : <Blog />, children : [
+            { path : 'php', element : <PhpBlog />},
+            { path : 'js', element : <JsBlog />},
+            { path : 'react', element : <ReactBlog />}
+        ]},
+        { path : '/login' , element : <Login />},
+        { path : '/panel' , element : <Panel />},
+        { path : '/course/:courseId' , element : <Course />},
+    ])
+
+    return routes
 }
 export default App
