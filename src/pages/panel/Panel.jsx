c57bb2ca8d774951311a5ae140a150f13e5c2e4c
bahare <pondblue07@gmail.com>
2023-10-12 00:16:11 +0330

    to simulate Login in Panel.jsx

--- src/pages/panel/Panel.jsx
+++ src/pages/panel/Panel.jsx
@@ -1,10 +1,23 @@
+import { Navigate } from 'react-router-dom'
 import MyNavbar from '../../components/navbar/Navbar'
 import './Panel.css'
 function Panel(){
+
+    const isLogin = (username) => {
+        if(username == 'admin') return true
+        return false
+    }
+
     return(
         <>
-        <MyNavbar />
-        <h1>Panel Page</h1>
+        { isLogin('admin') ? (
+            <>
+            <MyNavbar />
+            <h1>Panel Page</h1>
+            </>
+        ) : (
+            <Navigate to='/' />
+        )}
         </>
     )
 }
