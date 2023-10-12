a0d08de9f143fc09810837cb816729c12a57ddee
bahare <pondblue07@gmail.com>
2023-10-12 23:56:43 +0330

    App.jsx modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
 import {BrowserRouter, Routes, Route, useRoutes} from 'react-router-dom'
-import Home from './pages/home/Home'
-import About from './pages/about/About'
-import Blog from './pages/blog/Blog'
-import PhpBlog from './components/blogItems/PhpBlog'
-import JsBlog from './components/blogItems/JsBlog'
-import ReactBlog from './components/blogItems/ReactBlog'
-import Login from './pages/login/Login'
-import Panel from './pages/panel/Panel'
-import Course from './pages/course/Course'
-import PrivateRoute from './components/PrivateRoute'
+import routes from './routes'
 
 function App(){
 
-    let routes = useRoutes([
-        { path : '/', element : <Home />},
-        { path : '/about', element : <About />},
-        { path : '/blog/*', element : <Blog />, children : [
-            { path : 'php', element : <PhpBlog />},
-            { path : 'js', element : <JsBlog />},
-            { path : 'react', element : <ReactBlog />}
-        ]},
-        { path : '/login' , element : <Login />},
-        { path : '/panel' , element : <PrivateRoute> <Panel /> </PrivateRoute> },
-        { path : '/course/:courseId' , element : <Course />},
-    ])
+    let router = useRoutes(routes)
 
-    return routes
+    return router
 }
 export default App
