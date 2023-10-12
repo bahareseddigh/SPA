240667cd62ba45ee6b825d77be4482aa59cf5b47
bahare <pondblue07@gmail.com>
2023-10-12 23:50:47 +0330

    Panel component modified

--- src/pages/panel/Panel.jsx
+++ src/pages/panel/Panel.jsx
@@ -1,24 +1,14 @@
-import { Navigate } from 'react-router-dom'
 import MyNavbar from '../../components/navbar/Navbar'
 import './Panel.css'
 function Panel(){
 
-    const isLogin = (username) => {
-        if(username == 'admin') return true
-        return false
-    }
+    
 
     return(
-        <>
-        { isLogin('admin') ? (
             <>
             <MyNavbar />
             <h1>Panel Page</h1>
             </>
-        ) : (
-            <Navigate to='/' />
-        )}
-        </>
     )
 }
 export default Panel
