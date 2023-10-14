7c2827577576ceab908877f799328e66e9f02b8a
bahare <pondblue07@gmail.com>
2023-10-14 23:21:23 +0330

    use useState for username in Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,18 +1,15 @@
+import { useState } from 'react'
 import MyNavbar from '../../components/navbar/Navbar'
 import './Login.css'
 function Login(){
+
+    let [username, setUsername] = useState('')
+
     return(
         <>
         <MyNavbar />
         <div class="login-page">
             <div class="form">
-                <form class="register-form">
-                <input type="text" placeholder="name"/>
-                <input type="password" placeholder="password"/>
-                <input type="text" placeholder="email address"/>
-                <button>create</button>
-                <p class="message">Already registered? <a href="#">Sign In</a></p>
-                </form>
                 <form class="login-form">
                 <input type="text" placeholder="username"/>
                 <input type="password" placeholder="password"/>
