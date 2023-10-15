92613798009d9e0888c1d7e4f2a3b56563c35a51
bahare <pondblue07@gmail.com>
2023-10-15 22:57:51 +0330

    remove isLogin function from PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,11 +3,6 @@ import { Navigate } from "react-router-dom"
 
 function PrivateRoute({children}){
 
-    const isLogin = (username) => {
-        if(username == 'admin') return true
-        return false
-    }
-
     return(
         <>
             { isLogin('admin') ? children : <Navigate to='/' /> }
